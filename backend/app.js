b0a8515bea6e2898ee72a7c0f32dda3e038de9e7
Talha <talhariaz324@gmail.com>
2022-12-20 11:29:11 +0500

    Frontedn runing done

--- backend/app.js
+++ backend/app.js
@@ -18,11 +18,11 @@ app.use(fileUpload());
 // Routes
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
-// const order = require("./routes/orderRoute");
+const order = require("./routes/orderRoute");
 // const payment = require("./routes/paymentRoute");
 app.use("/api/v1", product);
 app.use("/api/v1", user);
-// app.use("/api/v1", order);
+app.use("/api/v1", order);
 // app.use("/api/v1", payment);
 
 // app.use(express.static(path.join(__dirname, "../frontend/build")));
---
+++ frontend/src/components/layout/Header/Header.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { ReactNavbar } from "overlay-navbar";
+import logo from "../../../images/logo.png";
+import { FaSearch, FaUserCircle } from "react-icons/fa";
+import { FiShoppingBag } from "react-icons/fi";
+
+const Header = () => {
+  return (
+    <ReactNavbar
+      burgerColorHover="#eb4034"
+      logo={logo}
+      logoWidth="20vmax"
+      navColor1="white"
+      logoHoverSize="10px"
+      logoHoverColor="#eb4034"
+      link1Text="Home"
+      link2Text="Products"
+      link3Text="Contact"
+      link4Text="About"
+      link1Url="/"
+      link2Url="/products"
+      link3Url="/contact"
+      link4Url="/about"
+      link1Size="1.3vmax"
+      link1Color="rgba(35, 35, 35,0.8)"
+      nav1justifyContent="flex-end"
+      nav2justifyContent="flex-end"
+      nav3justifyContent="flex-start"
+      nav4justifyContent="flex-start"
+      link1ColorHover="#eb4034"
+      link1Margin="1vmax"
+      profileIcon={true}
+      ProfileIconElement={FaUserCircle}
+      profileIconUrl="/login"
+      profileIconColor="rgba(35, 35, 35,0.8)"
+      profileIconColorHover="#eb4034"
+      searchIcon={true}
+      SearchIconElement={FaSearch}
+      searchIconColor="rgba(35, 35, 35,0.8)"
+      searchIconColorHover="#eb4034"
+      cartIcon={true}
+      CartIconElement={FiShoppingBag}
+      cartIconColor="rgba(35, 35, 35,0.8)"
+      cartIconColorHover="#eb4034"
+      cartIconMargin="1vmax"
+    />
+  );
+};
+
+export default Header;
